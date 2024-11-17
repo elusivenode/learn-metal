d7380ebd9ad92dfdeaad3b9ce51086bbf4dfc28a
Hamish MacDonald <hamish.macdonald@neds.com>
2024-11-17 12:53:39 +1000

    basic working example

---
+++ hello-triangle/hello-triangle/hello_triangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_triangleApp.swift
+//  hello-triangle
+//
+//  Created by Hamish MacDonald on 17/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct hello_triangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
